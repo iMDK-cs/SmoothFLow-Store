e149ff9e6cbe687b9fd79c52f51f537575e70069
MDK <m7md.dk2@gmail.com>
2025-09-18 12:12:20 +0300

    fffff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Providers from "@/components/Providers";
 import { Cairo, Inter } from 'next/font/google';
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 const cairo = Cairo({ 
   subsets: ['latin', 'arabic'],
@@ -171,6 +172,7 @@ export default function RootLayout({
             `
           }}
         />
+        <SpeedInsights />
       </body>
     </html>
   );
